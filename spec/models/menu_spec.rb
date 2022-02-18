51505b1066da3510d4e126c72c3dcdb9dc16ea45
Clay Cribbs <lclay.cribbs@gmail.com>
2022-02-18 18:05:25 -0500

    Add Menu model.

---
+++ spec/models/menu_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Menu, type: :model do
+  describe '#validations' do
+    it 'is valid if required fields are present' do
+      menu = build(:menu)
+
+      expect(menu).to be_valid
+      expect(menu.errors).to be_empty
+    end
+
+    [ #required field
+      :title,
+      :restaurant_name,
+      :status
+    ].each do |required_field|
+      it "is invalid if #{required_field} is not present" do
+        menu = build(:menu)
+        menu.send("#{required_field}=", '')
+
+      puts menu.errors.inspect
+
+        expect(menu).not_to be_valid
+        expect(menu.errors).to have_key(required_field)
+      end
+
+      it "is invalid if #{required_field} is set to nil" do
+        menu = build(:menu)
+        menu.send("#{required_field}=", nil)
+
+      puts menu.errors.inspect
+
+        expect(menu).not_to be_valid
+        expect(menu.errors).to have_key(required_field)
+      end
+    end
+  end
+end
