6dec5e9edc93f4ff404d9197af2224d1f6cd23b0
Clay Cribbs <lclay.cribbs@gmail.com>
2022-02-18 18:56:20 -0500

    Create MenuItems (#3)
    * Remove debug statements that were accidently left.
    
    * Create MenuItem model.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
   factory(:menu) do
-      title           { Faker::Food.ethnic_category }
-      restaurant_name { Faker::Restaurant.name }
-      status          { 0 }
+    title           { Faker::Food.ethnic_category }
+    restaurant_name { Faker::Restaurant.name }
+    status          { 0 }
+  end
+
+  factory(:menu_item) do
+    menu        { create(:menu) }
+    title       { Faker::Food.dish }
+    price       { Faker::Commerce.price }
+    description { Faker::Food.description }
+    status      { 0 }
   end
 end
