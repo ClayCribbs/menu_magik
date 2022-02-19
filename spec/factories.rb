6a3c825567ae62525abcfcbe7f383c747456fe7a
Clay Cribbs <lclay.cribbs@gmail.com>
2022-02-19 09:49:13 -0500

    Squash merge add-title-uniqueness-to-menu-items (#6)
    * Add uniqueness constraint on title for menus, scoped to restaurant.
    
    * Fix factory traits, add some basic tests to test association behavior.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,43 +1,44 @@
 FactoryBot.define do
   factory(:menu) do
-    restaurant      { create(:restaurant) }
-    status          { 0 }
-    title           { Faker::Food.ethnic_category }
+    restaurant { create(:restaurant) }
+    status     { 0 }
+    title      { Faker::Food.ethnic_category }
 
     trait :with_menu_items do
-      defaults do
+      transient do
         ## This can be passed to factory bot as a param to make as many items as you would like
-        number_of_menu_items = 1
+        number_of_menu_items { 1 }
       end
 
-      after(:build) do |menu, evaluator|
-        evaluator.number_of_menu_items.times do
-          menu.menu_items << FactoryGirl.create(:menu_item)
+      after(:create) do |menu, options|
+        options.number_of_menu_items.times do
+          FactoryBot.create(:menu_assignment, menu: menu)
         end
       end
     end
   end
 
   factory(:menu_assignment) do
-    menu { create(:menu) }
+    menu      { create(:menu) }
     menu_item { create(:menu_item) }
   end
 
   factory(:menu_item) do
+    restaurant  { create(:restaurant) }
     description { Faker::Food.description }
     price       { Faker::Commerce.price }
     status      { 0 }
     title       { Faker::Food.dish }
 
     trait :with_menus do
-      defaults do
+      transient do
         ## This can be passed to factory bot as a param to make as many menus as you would like
-        number_of_menus = 1
+        number_of_menus { 1 }
       end
 
-      after(:build) do |menu_item, evaluator|
-        evaluator.number_of_menus.times do
-          menu_item.menus << FactoryGirl.create(:menu)
+      after(:create) do |menu_item, options|
+        options.number_of_menus.times do
+          menu_item.menus << FactoryBot.create(:menu_assignment, menu_item: menu_item)
         end
       end
     end
@@ -54,24 +55,24 @@ FactoryBot.define do
     street_address { Faker::Address.street_address }
 
     trait :with_empty_menus do
-      defaults do
+      transient do
         ## This can be passed to factory bot as a param to make as many menus as you would like
-        number_of_menus = 1
+        number_of_menus { 1 }
       end
 
-      after(:build) do |restaurant|
-        restaurant.menus << FactoryGirl.create(:menu)
+      after(:create) do |restaurant|
+        restaurant.menus << FactoryBot.create(:menu, restaurant: restaurant)
       end
     end
 
     trait :with_populated_menus do
-      defaults do
+      transient do
         ## This can be passed to factory bot as a param to make as many menus as you would like
-        number_of_menus = 1
+        number_of_menus { 1 }
       end
 
-     after(:build) do |restaurant|
-        restaurant.menus << FactoryGirl.create(:men, :with_menu_items)
+     after(:create) do |restaurant|
+        restaurant.menus << FactoryBot.create(:menu, :with_menu_items, restaurant: restaurant)
       end
     end
   end
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe MenuItem, type: :model do
 
     [ #required field
       :price,
+      :restaurant_id,
       :status,
       :title,
     ].each do |required_field|
@@ -30,12 +31,81 @@ RSpec.describe MenuItem, type: :model do
         expect(menu_item.errors).to have_key(required_field)
       end
     end
+
+    context 'with same title as an existing MenuItem' do
+      let(:existing_menu_item) { FactoryBot.create(:menu_item, title: 'Example') }
+
+      context 'that shares the same restaurant' do
+        it 'is not valid' do
+          menu_item = build(:menu_item,
+                             title: 'Example',
+                             restaurant: existing_menu_item.restaurant)
+
+          expect(menu_item).not_to be_valid
+          expect(menu_item.errors).to have_key(:title)
+        end
+      end
+
+      context 'that does not share the same restaurant' do
+        it 'is valid' do
+          menu_item = build(:menu_item,
+                             title: 'Example',
+                             restaurant: FactoryBot.create(:restaurant))
+
+          expect(menu_item).to be_valid
+          expect(menu_item.errors).to be_empty
+        end
+      end
+    end
   end
 
   context 'associations' do
-    it 'belongs to menu' do
-      association_to_menu = MenuItem.reflect_on_association(:menus)
-      expect(association_to_menu.macro).to eq(:has_many)
+    it 'has_many menus' do
+      association_to_menus = MenuItem.reflect_on_association(:menus)
+      expect(association_to_menus.macro).to eq(:has_many)
+    end
+
+    it 'belongs to restaurant' do
+      association_to_restaurant = MenuItem.reflect_on_association(:restaurant)
+      expect(association_to_restaurant.macro).to eq(:belongs_to)
+    end
+  end
+
+  context 'behavior' do
+    context 'when associated with a single menu' do
+      let(:menu1)       { FactoryBot.create(:menu, :with_menu_items, number_of_menu_items: 3) }
+      let(:menu2)       { FactoryBot.create(:menu, restaurant: menu1.restaurant) }
+      let!(:menu_item1) { menu1.menu_items.first }
+
+      it 'can be added to another menu' do
+        expect(menu_item1.menus.count).to eq(1)
+
+        menu2.menu_items << menu_item1
+
+        expect(menu_item1.menus.count).to eq(2)
+        expect(menu1.menu_items).to include(menu_item1)
+        expect(menu2.menu_items).to include(menu_item1)
+      end
+    end
+
+    context 'when associated with multple menus' do
+      let(:menu1)       { FactoryBot.create(:menu, :with_menu_items, number_of_menu_items: 3) }
+      let(:menu2)       { FactoryBot.create(:menu, restaurant: menu1.restaurant) }
+      let!(:menu_item1) { menu1.menu_items.first }
+
+      before do
+        menu2.menu_items << menu_item1
+      end
+
+      it 'can be removed from a menu' do
+        expect(menu_item1.menus.count).to eq(2)
+
+        menu2.menu_items.delete(menu_item1)
+
+        expect(menu_item1.menus.count).to eq(1)
+        expect(menu1.menu_items).to include(menu_item1)
+        expect(menu2.menu_items).not_to include(menu_item1)
+      end
     end
   end
 end
