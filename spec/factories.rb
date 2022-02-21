344449ca01d00554bb5023ab2aba087c66dff96b
Clay Cribbs <lclay.cribbs@gmail.com>
2022-02-20 22:47:16 -0500

    Clean up factories.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,17 @@
 FactoryBot.define do
   factory(:menu) do
-    restaurant { create(:restaurant) }
+    restaurant  { create(:restaurant) }
     status     { 0 }
     title      { Faker::Food.ethnic_category }
 
     trait :with_menu_items do
       transient do
-        ## This can be passed to factory bot as a param to make as many items as you would like
         number_of_menu_items { 1 }
       end
 
       after(:create) do |menu, options|
         options.number_of_menu_items.times do
-          FactoryBot.create(:menu_assignment, menu: menu)
+          menu.menu_items << FactoryBot.create(:menu_item)
         end
       end
     end
@@ -32,13 +31,12 @@ FactoryBot.define do
 
     trait :with_menus do
       transient do
-        ## This can be passed to factory bot as a param to make as many menus as you would like
         number_of_menus { 1 }
       end
 
       after(:create) do |menu_item, options|
         options.number_of_menus.times do
-          menu_item.menus << FactoryBot.create(:menu_assignment, menu_item: menu_item)
+          menu_item.menus << FactoryBot.create(:menu)
         end
       end
     end
@@ -56,23 +54,21 @@ FactoryBot.define do
 
     trait :with_empty_menus do
       transient do
-        ## This can be passed to factory bot as a param to make as many menus as you would like
         number_of_menus { 1 }
       end
 
       after(:create) do |restaurant|
-        restaurant.menus << FactoryBot.create(:menu, restaurant: restaurant)
+        restaurant.menus << FactoryBot.create(:menu)
       end
     end
 
     trait :with_populated_menus do
       transient do
-        ## This can be passed to factory bot as a param to make as many menus as you would like
         number_of_menus { 1 }
       end
 
       after(:create) do |restaurant|
-        restaurant.menus << FactoryBot.create(:menu, :with_menu_items, restaurant: restaurant)
+        restaurant.menus << FactoryBot.create(:menu, :with_menu_items)
       end
     end
   end
