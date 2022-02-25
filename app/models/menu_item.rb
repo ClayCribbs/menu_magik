75049b239b946fdf3bc8469c454c4e6d588a9c1f
Clay Cribbs <lclay.cribbs@gmail.com>
2022-02-25 02:05:20 -0500

    Add dependent destroy to associations that need it.

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,6 @@
 class MenuItem < ApplicationRecord
   belongs_to :restaurant
-  has_many :menu_item_representations
+  has_many :menu_item_representations, dependent: :destroy
   has_many :menus, through: :menu_item_representations
 
   validates_presence_of :price, :restaurant_id, :status, :title
