4109c87ca4c9e0088335343dee8122d0d9b75d08
Clay Cribbs <lclay.cribbs@gmail.com>
2022-02-25 02:04:34 -0500

    Add methods to help with adding representations to orders.

--- app/models/MenuItemRepresentations/menu_item_representation.rb
+++ app/models/MenuItemRepresentations/menu_item_representation.rb
@@ -13,6 +13,26 @@ class MenuItemRepresentation < ApplicationRecord
     order.create_order_items_from_menu_item_representation(self)
   end
 
+  def create_child_from_menu_item(menu_item)
+    child_representation           = self.dup
+    child_representation.parent    = self
+    child_representation.menu_item = menu_item
+    child_representation.save!
+    child_representation
+  end
+
+  def copy_self_and_descendents_to(menu_item_representation)
+    new_representation = self.dup
+    new_representation.parent = menu_item_representation
+    new_representation.save!
+
+    if self.children.present?
+      self.children.each do |child|
+        child.copy_self_and_descendents_to(new_representation)
+      end
+    end
+  end
+
   def set_default_price_adjustment
     self.price_adjustment ||= menu_item.try(:price) || 0
   end
